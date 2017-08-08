1315c6ce61d3cabcd3ac927054633a9f45ee05e8
jnk5y <johnkyrus@gmail.com>
2017-08-07 22:54:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
+#!/usr/bin/env node
+'use strict';
+
 var express = require('express');
-
 var app = express();
-
 var spawn = require('child_process').spawn;
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+var https = require('https');
+var port = 8888;
+var server;
+var options = {
+        key: fs.readFileSync(path.join(__dirname, 'certs', 'server', 'privkey.pem')),
+        cert: fs.readFileSync(path.join(__dirname, 'certs', 'server', 'fullchain.pem'))
+};
 
-app.get('/invoke', function(req, res) {
-  const ls = spawn('/opt/script.py');
+// URL will look like - https://username:password@example.com:8888/trigger/action
+// Works with curl but a browser doesn't pass the username and password header correctly
+var expectedUsername = 'ENTER THE USERNAME YOU WANT TO USE';
+var expectedPassword = 'ENTER THE PASSWORD YOU WANT TO USE';
 
-  ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-    res.json({response: `output: ${data}`});
-  });
+app.get('/:trigger/:action', function(req,res) {
+        var header = req.headers['authorization'] || '',
+                token = header.split(/\s+/).pop() || '',
+                auth = new Buffer(token, 'base64').toString(),
+                parts = auth.split(/:/),
+                username = parts[0],
+                password = parts[1];
 
-  ls.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-  });
+        if(username == expectedUsername && password == expectedPassword) {
 
-  ls.on('close', (code) => {
-    console.log(`invoked script exited with code ${code}`);
-  });
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Authorized');
+
+                var action = req.params.action;
+                //console.log('action ='+action);
+                var trigger = req.params.trigger;
+
+                if (trigger == 'roku') {
+                        spawn(path.join(__dirname,'scripts','roku_trigger.py'), [action]);
+                        console.log('Roku Triggered');
+                }
+                else if (trigger == 'garage') {
+                        spawn(path.join(__dirname,'scripts','pi_garage_trigger.py'), [action]);
+                        console.log('Garage Triggered');
+                }
+        }
+        else {
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Not Authorized!');
+        }
+
 })
 
-app.listen(8888);
+server = https.createServer(options, app).listen(port, function() {
+        port = server.address().port;
+});
