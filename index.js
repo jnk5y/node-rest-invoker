6e23d09c9a9633c4ab9148dcc25083d80a0c459a
John Kyrus <jnk5y@yahoo.com>
2017-08-10 14:10:28 -0400

    check if the trigger file is in the scripts folder and then run it if it is

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
 #!/usr/bin/env node
 'use strict';
-
+var url = require('url');
 var express = require('express');
 var app = express();
 var spawn = require('child_process').spawn;
-var url = require('url');
 var path = require('path');
 var fs = require('fs');
 var https = require('https');
 var port = 8888;
 var server;
 var options = {
-        key: fs.readFileSync(path.join(__dirname, 'certs', 'server', 'privkey.pem')),
-        cert: fs.readFileSync(path.join(__dirname, 'certs', 'server', 'fullchain.pem'))
+	key: fs.readFileSync(path.join(__dirname, 'certs', 'server', 'privkey.pem')),	
+	cert: fs.readFileSync(path.join(__dirname, 'certs', 'server', 'fullchain.pem'))
 };
 
-// URL will look like - https://username:password@example.com:8888/trigger/action
-// Works with curl but a browser doesn't pass the username and password header correctly
-var expectedUsername = 'ENTER THE USERNAME YOU WANT TO USE';
-var expectedPassword = 'ENTER THE PASSWORD YOU WANT TO USE';
+var expectedUsername = 'johnkyrus';
+var expectedPassword = 'CkOUdM7r1O';
 
 app.get('/:trigger/:action', function(req,res) {
-        var header = req.headers['authorization'] || '',
-                token = header.split(/\s+/).pop() || '',
-                auth = new Buffer(token, 'base64').toString(),
-                parts = auth.split(/:/),
-                username = parts[0],
-                password = parts[1];
+	var header = req.headers['authorization'] || '',
+		token = header.split(/\s+/).pop() || '',
+		auth = new Buffer(token, 'base64').toString(),
+		parts = auth.split(/:/),
+		username = parts[0],
+		password = parts[1];
 
-        if(username == expectedUsername && password == expectedPassword) {
+	if(username == expectedUsername && password == expectedPassword) {
 
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Authorized');
-
-                var action = req.params.action;
-                var trigger = req.params.trigger;
-                
-                //ENTER YOUR LOGIC BASED ON TRIGGER AND ACTION VARIABLES
-        }
-        else {
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Not Authorized!');
-        }
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('authorized');
+		
+		var action = req.params.action;
+		var trigger = req.params.trigger;
+		var file = path.join(__dirname,'scripts', trigger +'_trigger.py');//filename);
+		
+		fs.stat(file, function(err,stat) {
+			if(err == null) { spawn(file, [action]); }
+			else { console.log('trigger not found'); }
+		});
+	}
+	else { 
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('Not authorized!');
+	}
 
 })
-
+	        
 server = https.createServer(options, app).listen(port, function() {
-        port = server.address().port;
+	port = server.address().port;
 });
