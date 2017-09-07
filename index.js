6131691e303e156cf30d1b058f976b68eac13d6e
John Kyrus <johnkyrus@gmail.com>
2017-09-07 10:48:29 -0400

    return output from spawned script

--- index.js
+++ index.js
@@ -24,22 +24,32 @@ app.get('/:clientKey/:trigger/:action', function(req,res) {
 	if(serverKey == null) { throw "SERVER_KEY env variable not set."; }
 
         if(clientKey == serverKey) {
-
+		var dataString = '';
                 res.setHeader('Content-Type', 'text/plain');
-                res.end('Authorized');
                 console.log('Authorized');
 
                 var file = path.join(__dirname,'node-rest-scripts', trigger +'_trigger.py');
 
                 fs.stat(file, function(err,stat) {
-                        if(err == null) { spawn(file, [action]); }
+                        if(err == null) { 
+				var output = spawn(file, [action]);
+				//read output from python script
+				output.stdout.on('data', function(data){
+                                        dataString += data.toString();
+                                });
+                                output.stdout.on('end', function(){
+                                        console.log(dataString);
+                                        res.end(dataString);
+                                });
+
+			}
                         else { console.log('Trigger script not found. Did you remember to attach your trigger volume to the docker run command with -v /your/triggers/folder:/usr/src/app/node-rest-scripts:Z'); }
                 });
 	}
 	else { 
 		res.setHeader('Content-Type', 'text/plain');
-		res.end('Not authorized!');
-		console.log('Not authorized!');
+		res.end('Not Authorized!');
+		console.log('Not Authorized!');
 	}
 
 })
